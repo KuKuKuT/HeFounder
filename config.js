47c36f25e917d0b6705790ec340ed954428aad92
TOXIC DEVIL <toxicdevil.abhinav@gmail.com>
2021-10-30 18:57:43 +0530

    New Var 🥳

--- config.js
+++ config.js
@@ -14,6 +14,7 @@ module.exports = {
     SESSION: process.env.ALEXA_SESSION === undefined ? '' : process.env.ALEXA_SESSION,
     ANTILINK: process.env.ANTI_LINK === undefined ? 'false' : process.env.ANTI_LINK,
     FULLALEXA: process.env.FULL_ALEXA === undefined ? 'false' : process.env.FULL_ALEXA,
+    LOG: process.env.LOG === undefined ? 'true' : process.env.LOG,
     OWNER: process.env.OWNER_NAME === undefined ? 'TOXIC DEVIL' : process.env.OWNER_NAME,
     OWNERNUM: process.env.OWNER_NUMBER === undefined ? '94768826133' : process.env.OWNER_NUMBER,
     AUTOBIO: process.env.AUTO_BIO === undefined ? 'false' : process.env.AUTO_BIO,
